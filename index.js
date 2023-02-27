c9d0e282c29f4eb03bd22cae9e11ac3c20e5ae66
kale <42823429+oelin@users.noreply.github.com>
2023-02-27 14:35:41 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+export const element = name => document.createElement(name)
+
+HTMLElement.prototype.element = function(name) { return this.appendChild(document.createElement(name)) }
+HTMLElement.prototype.text = function(text) { return (this.innerText = text, this) }
+HTMLElement.prototype.html = function(html) { return (this.innerHTML = html, this) }
+HTMLElement.prototype.set = function(...a) { return (this.setAttribute(...a), this) }
+HTMLElement.prototype.on = function(...a) { return (this.addEventListener(...a), this) }
+HTMLElement.prototype.end = function() { return this.parentElement }
+HTMLElement.prototype.top = function() { return this.getRootNode() }
