5786c8bd2c6a0f2be08cc7de8949d96f0441b454
kale <42823429+oelin@users.noreply.github.com>
2023-02-27 18:36:25 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ export const Element = name => document.createElement(name)
 export const use = (name, value) => HTMLElement.prototype[name] = value
 
 
-// Default plug-ins.
+// Default plugins.
 
-use('Element', function(name) { return this.appendChild(document.createElement(name)) }
-use('attribute', function(name, value) { return (this.setAttribute(name, value), this) }
-use('event', function(name, value) { return (this.addEventListener(name, value), this) }
-use('text', function(value) { return (this.innerText = value, this) }
-use('end', function() { return this.parentElement }
-use('top', function() { return this.getRootNode() }
+use('Element', function(name) { return this.appendChild(document.createElement(name)) })
+
+use('attribute', function(name, value) { return (this.setAttribute(name, value), this) })
+
+use('event', function(name, value) { return (this.addEventListener(name, value), this) })
+
+use('text', function(value) { return (this.innerText = value, this) })
+
+use('end', function() { return this.parentElement })
+
+use('top', function() { return this.getRootNode() })
